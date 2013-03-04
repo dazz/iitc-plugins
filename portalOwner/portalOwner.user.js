009952081f4fe67c653803f4721335787c8fdc04
sMiLe <smile@c3pb.de>
2013-03-04 14:57:08 +0100

    more bug fixes and cleanup

--- portalOwner/portalOwner.user.js
+++ portalOwner/portalOwner.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @id             portalOwner@smilodazsprod
-// @name           iitc: PortalOwner
+// @name           iitc: portalOwner
 // @version        0.1
-// @updateURL      https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/plugins/max-links.user.js
-// @downloadURL    https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/plugins/max-links.user.js
-// @description    Calculates how to link the portals to create the maximum number of fields.
+// @updateURL      https://raw.github.com/dazz/iitc-plugins/gh-pages/plugins/portalOwner.user.js
+// @downloadURL    https://raw.github.com/dazz/iitc-plugins/gh-pages/plugins/portalOwner.user.js
+// @description    Shows red marker where player is owner of a portal and a yellow marker if player has resos in a portal
 // @include        https://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
 // ==/UserScript==
@@ -23,7 +23,7 @@ window.plugin.portalOwner = function() {};
 // const values
 window.plugin.portalOwner.OWNER_COLOR = "red";
 window.plugin.portalOwner.RESO_COLOR = "yellow";
-    
+
 window.plugin.portalOwner._delaunayScriptLocation = 'https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/dist/delaunay.js';
 
 window.plugin.portalOwner.layer = null;
@@ -32,19 +32,19 @@ window.plugin.portalOwner._updating = false;
 window.plugin.portalOwner._renderLimitReached = false;
 
 window.plugin.portalOwner.updateLayer = function() {
-  if (window.plugin.portalOwner._updating ||
-      window.plugin.portalOwner.layer === null ||
-      !window.map.hasLayer(window.plugin.portalOwner.layer))
+  if (!window.map.hasLayer(window.plugin.portalOwner.layer) || window.plugin.portalOwner._updating ||
+      window.plugin.portalOwner.layer === null) {
     return;
+  }
   window.plugin.portalOwner._updating = true;
   window.plugin.portalOwner.layer.clearLayers();
 
   var locations = [];
   var minX = 0;
   var minY = 0;
-  
-  var base = 'http://breunigs.github.com/ingress-intel-total-conversion/dist/images/';
-  base = 'http://smile.crew.c-base.org/images/';
+
+  var base = 'https://breunigs.github.com/ingress-intel-total-conversion/dist/images/';
+  base = 'https://smile.crew.c-base.org/images/';
   L.Icon.Default.imagePath = base;
 
   window.iconRed = L.Icon.Default.extend({options: { iconUrl: base + 'marker-red.png' } });
@@ -54,10 +54,8 @@ window.plugin.portalOwner.updateLayer = function() {
   var iconYellow = new window.iconYellow();
 
   $.each(window.portals, function(guid, portal) {
-
     var nick = PLAYER.nickname;
-    //nick = "ihmisSuski";
-   
+
     if(getPlayerName(portal.options.details.captured.capturingPlayerId) === nick){
       var m = L.marker([portal._latlng.lat, portal._latlng.lng], {title: nick, clickable: false, icon: icon});
       // ensure tooltips are closed, sometimes they linger
@@ -65,25 +63,24 @@ window.plugin.portalOwner.updateLayer = function() {
       m.addTo(window.plugin.portalOwner.layer);
       // jQueryUI doesn’t automatically notice the new markers
       window.setupTooltips($(m._icon));
-        
     } else {
-    $.each(portal.options.details.resonatorArray.resonators, function(ind, reso) {
-      if(reso !== null) {
-                    if(getPlayerName(reso.ownerGuid) === nick) {
-                  var m = L.marker([portal._latlng.lat, portal._latlng.lng], {title: nick, clickable: false, icon: iconYellow});
-      m.on('mouseout', function() { $on(this._icon).tooltip('close'); });
-                  m.addTo(window.plugin.portalOwner.layer);
+      $.each(portal.options.details.resonatorArray.resonators, function(ind, reso) {
+        if(reso !== null) {
+          if(getPlayerName(reso.ownerGuid) === nick) {
+            var m = L.marker([portal._latlng.lat, portal._latlng.lng], {title: nick, clickable: false, icon: iconYellow});
+            m.on('mouseout', function() { $on(this._icon).tooltip('close'); });
+            m.addTo(window.plugin.portalOwner.layer);
                   // jQueryUI doesn’t automatically notice the new markers
-                  window.setupTooltips($(m._icon));
-                }
-            }
-    });
-    }_icon
+            window.setupTooltips($(m._icon));
+          }
+        }
+      });
+    }
   });
   window.plugin.portalOwner._updating = false;
   window.renderUpdateStatus();
-    
-}
+
+};
 
 window.plugin.portalOwner.setup = function() {
   load(window.plugin.portalOwner._delaunayScriptLocation).thenRun(function() {
@@ -109,7 +106,7 @@ window.plugin.portalOwner.setup = function() {
     window.layerChooser.addOverlay(window.plugin.portalOwner.layer, 'My Portals');
     map.addLayer(window.plugin.portalOwner.layer);
   });
-}
+};
 var setup = window.plugin.portalOwner.setup;
 
 // PLUGIN END //////////////////////////////////////////////////////////
@@ -128,4 +125,5 @@ if(window.iitcLoaded && typeof setup === 'function') {
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('('+ wrapper +')();'));
 (document.body || document.head || document.documentElement).appendChild(script);
+
 
